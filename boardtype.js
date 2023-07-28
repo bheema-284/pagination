3ffa02001a57318cb5ab30cfe59e4f9664a4d898
Bheemudu Guguloth <96168073+bheema-284@users.noreply.github.com>
2023-07-28 18:05:01 +0530

    Update

--- boardtype.js
+++ boardtype.js
@@ -1,63 +1,54 @@
-import { useEffect, useRef, useState } from "react";
-import useSWR, { useSWRConfig } from "swr";
-import Error from "../../components/common/error";
-import Loading from "../../components/common/loading";
-import { CMS_SERVICE_URL, getServiceHeader, trackerror } from "../../config/sitesettings";
-import Notificationalert from "../../components/common/notificationalert";
-import { showNotificationAlert } from "../../config/globalfunctions";
+import { useRef, useState } from "react";
 import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/20/solid";
-import { useRouter } from "next/router";
-import BoardTypeList from "../../components/boardtype/boardtypelist";
-import AddBoardType from "../../components/boardtype/addboardtype";
-import BoardTypeTab from "../../components/boardtype/boardtypedetailstab";
-export default function BoardType() {
-
-    const router = useRouter()
-
-    const GetServiceObj = () => {
-        let token = getServiceHeader();
-        if (!token) {
-            if (typeof window !== 'undefined') {
-                router.push({
-                    pathname: "/login",
-                });
-            }
-            return false;
-        } else {
-            return token
-        }
-    };
-
-    const fetcher = async (url) =>
-        fetch(url, { headers: await GetServiceObj() }).then((res) => {
-            if (res.status === 200) {
-                return res.json()
-            } else {
-                let error = true;
-                trackerror(error)
-                throw error
-            }
-        }
-        );
-
-    const { mutate } = useSWRConfig();
-
-    const [screen, setScreen] = useState("boardtype");
-    const [searchText, setSearchText] = useState("");
-    const [editData, setEditData] = useState({});
-    const [boardTypeId, setBoardTypeId] = useState("")
-    const [changeMode, setChangeMode] = useState("create");
+export default function MultiSearch() {
+    const data =[
+    {
+        "_id": "63a2930070399f8e3528ebce",
+        "code": "IDS",
+        "translations": {
+            "language": "en",
+            "name": "Hikura",
+            "description": "This is a new boardtype."
+        },
+        "created_by_id": "6352492baa2ef07702408ecc",
+        "created_by_name": "Saikrishna Vedagiri",
+        "created_on": "2022-12-21T05:00:48.267Z",
+        "modified_by_id": "6351162aa473c693baea6113",
+        "modified_by_name": "Bheema Test Guguloth",
+        "modified_on": "2023-06-30T11:05:04.752Z"
+    },
+    {
+        "_id": "63a2933c70399f8e3528ebcf",
+        "code": "UDH",
+        "translations": {
+            "language": "en",
+            "name": "Terra",
+            "description": "This is an old board type."
+        },
+        "created_by_id": "6352492baa2ef07702408ecc",
+        "created_by_name": "Saikrishna Vedagiri",
+        "created_on": "2022-12-21T05:01:48.514Z",
+        "modified_by_id": "6351162aa473c693baea6113",
+        "modified_by_name": "Bheema Guguloth",
+        "modified_on": "2022-12-21T09:30:29.699Z"
+    },
+    {
+        "_id": "63a2ac006a05403d182211b7",
+        "code": "EWDEW",
+        "translations": {
+            "language": "en",
+            "name": "New Board",
+            "description": "board type board type  board type ecvfehnb gfh dfgh"
+        },
+        "created_by_id": "6351162aa473c693baea6113",
+        "created_by_name": "Bheema Guguloth",
+        "created_on": "2022-12-21T06:47:07.577Z",
+        "modified_by_id": "6351162aa473c693baea6113",
+        "modified_by_name": "Bheema Test Guguloth",
+        "modified_on": "2023-05-18T12:27:44.217Z"
+    }
+]
     const [searchResults, setSearchResults] = useState([]);
-    const [ShowNotificationAlert, setShowNotificationAlert] = useState(false);
-    const [NotificationDetails, setNotificationDetails] = useState({
-        isError: false,
-        isSuccess: false,
-        isWarning: false,
-        message: "",
-    });
-    const inputRef = useRef()
-    const { data, error } = useSWR(CMS_SERVICE_URL + `hotels/boardtype`, fetcher, { revalidateOnFocus: false, shouldRetryOnError: false });
-
     const searchData = (query) => {
         // return Array.isArray(item) && item.filter((i) => i.translations.name.toLowerCase().includes(searchText))
         const filteredResults = data?.filter(
@@ -86,67 +77,7 @@ export default function BoardType() {
             setSearchText("")
             searchData("")
         }
-    }
-    const cancel = () => {
-        setScreen("boardtype");
-        setChangeMode("create");
-        if (changeMode === "update") {
-            router.push({
-                pathname: `/masters/boardtype`,
-                query: { id: boardTypeId }
-            })
-            setScreen("details");
-        }
-        else {
-            router.push({
-                pathname: `/masters/boardtype`,
-            })
-        }
-    };
-    const SelectMode = (type) => {
-        setChangeMode(type);
-    };
-    const addForm = () => {
-        setScreen("addBoardType");
-        SelectMode("create");
-    }
-    const editForm = (item, mode) => {
-        setScreen("addBoardType");
-        setEditData(item);
-        setChangeMode(mode)
-    };
-    const showDetails = (id) => {
-        router.push({
-            pathname: `/masters/boardtype`,
-            query: { id: id }
-        })
-        setBoardTypeId(id);
-        setScreen("details");
-    };
-
-    useEffect(() => {
-        if (router.query.id) {
-            setBoardTypeId(router.query.id)
-            setScreen("details");
-        }
-    }, [router.query.id])
-
-    const showNotification = (status) => {
-        if (status === 200) {
-            showNotificationAlert(
-                setShowNotificationAlert,
-                setNotificationDetails({
-                    isSuccess: true,
-                    message: "Success!",
-                })
-            );
-        }
-    }
-
-    const mutated = () => {
-        mutate(CMS_SERVICE_URL + `hotels/boardtype`);
-    };
-
+    }   
     return (
         <div className="h-full">
             {screen === "boardtype" ? (
@@ -181,36 +112,51 @@ export default function BoardType() {
                     </div>
                     {error ? <Error /> : !data ? <Loading /> : null}
                     {data ? (
-                        <BoardTypeList
-                            data={newData}
-                            showDetails={showDetails}
-                            showNotification={showNotification}
-                        />
+                        <div className="px-4 md:px-6 lg:px-0 mb-20">
+                <div className="mt-8 flex flex-col">
+                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle lg:px-8">
+                            <div className="overflow-y-visible shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-300">
+                                    <thead className="bg-gray-50">
+                                        <tr>
+                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Code
+                                            </th>
+                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Name
+                                            </th>
+                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Description
+                                            </th>
+                                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                <span className="sr-only"></span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="divide-y divide-gray-200 bg-white">
+                                        {data?.map((item, index) => (
+                                            <tr key={index}>
+                                                <td className="whitespace-nowrap px-3 py-4 text-sm ">{item.code}</td>
+                                                <td className="whitespace-nowrap px-3 py-4 text-sm ">{item.translations.name}</td>
+                                                <td className="whitespace-nowrap px-3 py-4 text-sm ">{item.translations.description}</td>
+                                                <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <button onClick={() => showDetails(item._id)} className="text-indigo-600 hover:text-indigo-900">
+                                                        Details
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
                     ) : null}
                 </div>
-            ) : screen === "addBoardType" ? (
-                <AddBoardType
-                    cancel={cancel}
-                    showNotification={showNotification}
-                    changeMode={changeMode}
-                    mutated={mutated}
-                    editData={changeMode === "create" ? "" : editData}
-                />
-            ) : screen === "details" && boardTypeId ? (
-                <BoardTypeTab
-                    cancel={cancel}
-                    boardTypeId={boardTypeId}
-                    editForm={editForm}
-                />
             ) : null}
-            {ShowNotificationAlert && (
-                <Notificationalert
-                    message={NotificationDetails.message}
-                    success={NotificationDetails.isSuccess}
-                    error={NotificationDetails.isError}
-                    warning={NotificationDetails.isWarning}
-                />
-            )}
         </div>
     );
 }
